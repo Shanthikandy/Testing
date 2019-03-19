da1f94935d2e57b3b67089c35fa376124cb4fc12
Shanthikandy <48706371+Shanthikandy@users.noreply.github.com>
2019-03-19 10:47:51 +0530

    Create Webs.java

---
+++ Webs.java
@@ -0,0 +1,24 @@
+package Packs;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Webs {
+	WebDriver driver = new FirefoxDriver();
+	
+	@BeforeTest
+	public void practice()
+	{
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\ShanthiKandasamy\\Documents\\geckodriver.exe");
+		
+    }
+	@Test
+	 public void pracs()
+	 {
+		driver.get("http://www.qaclickacademy.com/");
+	 }
+	
+
+}
